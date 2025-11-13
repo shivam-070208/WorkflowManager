7437f235d0609ea241525f412ce334e75e8709aa
shvgpt-070208 <shvgpt070208@gmail.com>
2025-11-14 02:50:48 +0530

    prisma

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+const globalPrisma = global as unknown as {
+    prisma:PrismaClient;
+}
+
+
+const prisma = globalPrisma.prisma || new PrismaClient();
+
+
+if(process.env.NODE_ENV=="production") globalPrisma.prisma = prisma;
